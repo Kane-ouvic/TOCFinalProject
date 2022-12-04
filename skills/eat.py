c42eeba526212c72b2f76eae45cfb1dfcddc1679
ouvic <ouvic215@gmail.com>
2022-12-04 16:21:11 +0800

    Update eat.py

--- skills/eat.py
+++ skills/eat.py
@@ -5,21 +5,27 @@ from models.message_request import MessageRequest
 from skills import add_skill
 import random
 
+
 @add_skill('/吃什麼')
 def get(message_request: MessageRequest):
-    
-    foods_str = message_request.message.replace("/吃什麼", "")
+
+    # /吃什麼 烤雞.炸雞.鹹酥雞
+
+    # 處理字串
+    foods_str = message_request.message.replace("/吃什麼 ", "")
     foods = foods_str.split('.')
     print(foods)
-    
+
+    # 隨機挑選一個項目
     result = random.choice(foods)
     print(result)
-    
+
+    # 處理回傳訊息
     message = TemplateSendMessage(
         alt_text='今天吃什麼',
         template=ButtonsTemplate(
             title='今天吃什麼?',
-            text=f'就決定吃{result}吧!',
+            text=f'就決定吃 {result} 吧!',
             actions=[
                 MessageAction(
                     label='我不要，換一個',
@@ -28,6 +34,7 @@ def get(message_request: MessageRequest):
             ]
         )
     )
+
     return [
         message
     ]
