7b5df4046e6c78c394e5bfc1a725bce854453ded
ouvic <ouvic215@gmail.com>
2022-12-20 15:31:39 +0800

    upload fsm

--- skills/getfsm.py
+++ skills/getfsm.py
@@ -1,11 +1,13 @@
 from typing import Text
 from linebot.models import TextSendMessage
 from models.message_request import MessageRequest
+from linebot.models import ImageSendMessage
 from skills import add_skill
 
 
-@add_skill('/hello')
+@add_skill('/fsm')
 def get(message_request: MessageRequest):
     return [
-        TextSendMessage(text=f'Hello!!!!!!!!')
+        ImageSendMessage(original_content_url='https://i.imgur.com/YrovBZg.png',
+    preview_image_url='https://i.imgur.com/YrovBZg.png')
     ]
